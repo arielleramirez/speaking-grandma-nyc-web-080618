bf2b2ee120be618b3d13108522b0a08341394415
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-07-21 03:31:03 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,19 @@
+
+ 
+  # Check if string equals same phrase all upper case letters, which means string is all uppercase
+
+ def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
+  end
+end
+
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
